878b8c4693a36f075f2f8f8704f5f044f1553a6b
medacaou0003 <aou0003@alu.medac.es>
2024-10-17 13:35:15 +0200

    Update foodtrack.sql

--- foodtrack.sql
+++ foodtrack.sql
@@ -2,7 +2,7 @@
 CREATE database foodtrak;
 use foodtrak
 
-CREATE TABLE Forgoneta (
+CREATE TABLE Furgoneta (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     posicion VARCHAR(50),
@@ -25,8 +25,8 @@ CREATE TABLE Producto (
     categoria VARCHAR(50),
     precio DECIMAL(5,2) NOT NULL,
     ingredientes TEXT,
-    id_forgoneta INT,
-    FOREIGN KEY (id_forgoneta) REFERENCES Forgoneta(id)
+    id_furgoneta INT,
+    FOREIGN KEY (id_furgoneta) REFERENCES Furgoneta(id)
 );
 CREATE TABLE Pedido (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -34,12 +34,12 @@ CREATE TABLE Pedido (
     deporte VARCHAR(50),
     tipo_pago VARCHAR(50),
     fecha_hora DATETIME NOT NULL,
-    id_forgoneta INT,
+    id_furgoneta INT,
     observaciones TEXT,
-    FOREIGN KEY (id_forgoneta) REFERENCES Forgoneta(id)
+    FOREIGN KEY (id_furgoneta) REFERENCES Furgoneta(id)
 );
 
 
 ALTER TABLE Empleado
-ADD COLUMN id_forgoneta INT,
-ADD FOREIGN KEY (id_forgoneta) REFERENCES Forgoneta(id);
+ADD COLUMN id_furgoneta INT,
+ADD FOREIGN KEY (id_furgoneta) REFERENCES Furgoneta(id);
